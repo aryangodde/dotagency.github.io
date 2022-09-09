23313da0d27107af7fb2fc3847e96c3c4acd58e2
Stabby <47973917+aryangodde@users.noreply.github.com>
2022-09-10 01:30:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navlinks');
+
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        burger.classList.toggle('toggle');
+ 
+    });
+}
+
+navSlide();
